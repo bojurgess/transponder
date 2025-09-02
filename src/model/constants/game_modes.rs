74ec46018c2f4a214b1acb50d66c70ecf1eab08d
Joe Burgess <bojurgess@gmail.com>
2025-09-02 17:31:06 +0100

    define game_modes id appendix constant

---
+++ src/model/constants/game_modes.rs
@@ -0,0 +1,16 @@
+use crate::utils::define_appendix;
+
+define_appendix!(GameModeId {
+    4 => GrandPrix23 => "Grand Prix '23",
+    5 => TimeTrial => "Time Trial",
+    6 => Splitscreen,
+    7 => OnlineCustom => "Online Custom",
+    15 => OnlineWeeklyEvent => "Online Weekly Event",
+    17 => StoryModeBrakingPoint => "Story Mode (Braking Point)",
+    27 => MyTeamCareer25 => "My Team Career '25",
+    28 => DriverCareer25 => "Driver Career '25",
+    29 => Career25Online => "Career '25 Online",
+    30 => ChallengeCareer25 => "Challenge Career '25",
+    75 => StoryModeAPXGP => "Story Mode (APXGP)",
+    127 => Benchmark
+});
--- src/model/constants/mod.rs
+++ src/model/constants/mod.rs
@@ -1,9 +1,11 @@
 pub mod drivers;
+pub mod game_modes;
 pub mod nationalities;
 pub mod teams;
 pub mod tracks;
 
 pub use drivers::*;
+pub use game_modes::*;
 pub use nationalities::*;
 pub use teams::*;
 pub use tracks::*;
