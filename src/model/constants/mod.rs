1a0c00ee5a605661752818f884f0f82366e686f6
Joe Burgess <bojurgess@gmail.com>
2025-09-02 17:37:53 +0100

    define ruleset id appendix constant

--- src/model/constants/mod.rs
+++ src/model/constants/mod.rs
@@ -1,6 +1,7 @@
 pub mod drivers;
 pub mod game_modes;
 pub mod nationalities;
+pub mod rulesets;
 pub mod session_types;
 pub mod teams;
 pub mod tracks;
@@ -8,6 +9,7 @@ pub mod tracks;
 pub use drivers::*;
 pub use game_modes::*;
 pub use nationalities::*;
+pub use rulesets::*;
 pub use session_types::*;
 pub use teams::*;
 pub use tracks::*;
---
+++ src/model/constants/rulesets.rs
@@ -0,0 +1,8 @@
+use crate::utils::define_appendix;
+
+define_appendix!(RulesetId {
+    0 => PracticeAndQualifying => "Practice & Qualifying",
+    1 => Race,
+    2 => TimeTrial => "Time Trial",
+    12 => Elimination
+});
