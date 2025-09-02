fd323cbb9d3d71079da7c1830d115f30eceeb248
Joe Burgess <bojurgess@gmail.com>
2025-09-02 17:28:03 +0100

    define nationality id appendix constant, modify macro to allow omitting display name

--- src/model/constants/mod.rs
+++ src/model/constants/mod.rs
@@ -1,7 +1,9 @@
 pub mod drivers;
+pub mod nationalities;
 pub mod teams;
 pub mod tracks;
 
 pub use drivers::*;
+pub use nationalities::*;
 pub use teams::*;
 pub use tracks::*;
--- src/model/utils/macros.rs
+++ src/model/utils/macros.rs
@@ -1,35 +1,28 @@
 macro_rules! define_appendix {
-    ($name:ident { $($id:literal => $variant:ident => $display:expr),* $(,)? }) => {
-        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-        pub enum $name {
-            $(
-                $variant,
-            )*
-            Unknown(u8),
-        }
-
-        impl From<u8> for $name {
-            fn from(id: u8) -> Self {
-                match id {
-                    $(
-                        $id => $name::$variant,
-                    )*
-                    other => $name::Unknown(other),
-                }
-            }
-        }
+    ($name:ident { $($id:expr => $variant:ident $(=> $display:expr)?),* $(,)? }) => {
+        #[derive(Debug, Clone, Copy)]
+        pub enum $name { $($variant),* }
 
         impl $name {
             pub fn name(&self) -> &'static str {
                 match self {
-                    $(
-                        $name::$variant => $display,
-                    )*
-                    $name::Unknown(_) => "Unknown",
+                    $(Self::$variant => define_appendix!(@display $variant $(, $display)?)),*
+                }
+            }
+        }
+
+        impl From<u8> for $name {
+            fn from(value: u8) -> Self {
+                match value {
+                    $($id => Self::$variant),*,
+                    _ => panic!("Unknown {} ID: {}", stringify!($name), value),
                 }
             }
         }
     };
+
+    (@display $variant:ident , $display:expr) => { $display };
+    (@display $variant:ident) => { stringify!($variant) };
 }
 
 pub(crate) use define_appendix;
